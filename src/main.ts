d66d19510ac40b51b49401ab14b6e7c6779c7d82
Daniel Jírovec <danjirovec@gmail.com>
2024-09-25 23:16:15 +0200

    Add production cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173, https://applausio-fe.onrender.com'],
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
   });
