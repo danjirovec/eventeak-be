7ba12ee140387ae1a60a86e1b3b2292f3557e5a8
Daniel Jírovec <danjirovec@gmail.com>
2024-11-07 23:20:07 +0100

    1.0.1.1

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL, 'https://www.staging.eventeak.com'],
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT', 'PATCH'],
     credentials: true,
   });
