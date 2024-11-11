e0fadca7820b30d58e82dc6bd7278cb75a901910
Daniel Jírovec <danjirovec@gmail.com>
2024-11-12 00:37:47 +0100

    1.0.1.1m

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -17,28 +17,21 @@ export class AuthGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const ctx = GqlExecutionContext.create(context);
-    console.log('CONTEXT', ctx.getContext());
     const request = ctx.getContext().req;
     try {
-      console.log('REQUEST', request);
       const token = this.getToken(request);
-      console.log('TOKEN', token);
       const user = this.jwtService.verify(token, {
         secret: this.configService.get<string>('JWT_SECRET'),
       });
-      console.log('USER', user);
       request.userId = user.sub;
       return true;
     } catch (error) {
-      console.log('ERROR', error);
       throw new UnauthorizedException();
     }
   }
 
   protected getToken(request: { headers: Record<string, string> }): string {
-    console.log('HEADERS', request.headers);
     const token = request.headers['authorization'];
-    console.log('TOKEN BEFORE SPLIT', token);
     const [_, _token] = token.split(' ');
     return _token;
   }
